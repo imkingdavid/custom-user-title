ad55937f2ef012982f0fa3566fcc25ce04ee2133
Mauron <Mauron@users.noreply.github.com>
2015-11-27 20:12:17 +0100

    Update for Italian translation for permissions_customusertitle.php

--- language/it/permissions_customusertitle.php
+++ language/it/permissions_customusertitle.php
@@ -37,5 +37,5 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'ACL_U_USER_CUSTOM_TITLE'		=> 'Può impostare un livello personale',
+	'ACL_U_USER_CUSTOM_TITLE'		=> 'Può impostare un titolo personale',
 ));
